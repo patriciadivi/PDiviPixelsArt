96ec9a930c6d431a7435be28b70f401eb4a790f6
PatriciaDivi <patriciavianad@gmail.com>
2021-12-09 19:27:45 -0300

    2 Requisito | Adicione à página uma paleta contendo quatro cores distintas.  | :rocket: | 3 Requisitos | Adicione a cor preta como a primeira cor da paleta de cores. | :rocket: |

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 //-----------------------------------------------------
-// Adicione à página o título "Paleta de Cores".
-let body = document.querySelector('body')
-let tagH1 = document.createElement('h1')
-console.log(tagH1);
+//1- Adicione à página o título "Paleta de Cores".
+let header = document.querySelector("header")
+let tagH1 = document.createElement("h1")
+// console.log(tagH1);
 tagH1.id = "title"
 
 let textTitle = document.createTextNode('Paleta de Cores') 
 
 tagH1.appendChild(textTitle)
-body.appendChild(tagH1)
+header.appendChild(tagH1)
 
 //-----------------------------------------------------
-// Adicione à página uma paleta contendo quatro cores distintas.
+//2 - Adicione à página uma paleta contendo quatro cores distintas.
+
+// let tagSection = document.querySelector("section")
+// tagSection.id = "color-palette"
+// // console.log(tagSection);
+
+
